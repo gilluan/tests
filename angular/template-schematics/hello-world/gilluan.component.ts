3d19a134cecd9e074ab49cf4ff0058c791efeb54
Gilluan Dutra Formiga Sousa <gilluan@hotmail.com.br>
2018-09-10 15:07:11 -0300

    corrigindo geração de arquivos iniciais

--- angular/template-schematics/hello-world/gilluan.component.ts
+++ angular/template-schematics/hello-world/gilluan.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '<%= selector %>',
+  selector: '',
   template: `
     <h1>Test schematics</h1>
-    <h2> <%= dasherize(name) %> funcionando!</h2>
+    <h2>dasherize(name) funcionando!</h2>
   `,
   styles: []
 })
-export class <%= classify(name) %>Component implements OnInit {
+export class GilluanComponent implements OnInit {
 
   constructor() { }
 
--- angular/template-schematics/hello-world/src/hello-world/my-component/index.ts
+++ angular/template-schematics/hello-world/src/hello-world/my-component/index.ts
@@ -1,5 +1,6 @@
-import { Rule, SchematicContext, Tree, apply, url } from '@angular-devkit/schematics';
-import { template } from '@angular-devkit/core';
+import { Rule, SchematicContext, Tree, apply, url, move, template, chain, branchAndMerge, mergeWith } from '@angular-devkit/schematics';
+import {  strings } from '@angular-devkit/core';
+
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
@@ -7,9 +8,18 @@ import { template } from '@angular-devkit/core';
 export function create(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     console.log('_options', _options);
- //   tree.create('test.txt', 'Hello World!');
 
- 
-    return tree;
+    const templateSrc = apply(url('./files'), [
+        template({
+            ...strings, ..._options
+        }),
+        move('./src/components')
+    ]);
+
+    return chain([
+        branchAndMerge(
+          chain([mergeWith(templateSrc)])
+        ),
+      ])(tree, _context);
   };
 }
