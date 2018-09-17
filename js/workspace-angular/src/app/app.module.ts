b9de0cf2ad522a590001881c4d78c99947861b2e
Gilluan Dutra Formiga Sousa <gilluan@hotmail.com.br>
2018-09-17 14:15:43 -0300

    Adicionando exemplo de monorepo com o lerna

--- js/workspace-angular/src/app/app.module.ts
+++ js/workspace-angular/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Lib1Module } from 'projects/lib1/src/public_api';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Lib1Module
   ],
   providers: [],
   bootstrap: [AppComponent]
